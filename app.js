5e6fdf216cb2e2b4f239befa52b3ec37de7c87df
RohitKewat <rohit554514@gmail.com>
2023-03-31 23:45:24 +0530

    added backend code

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express()
+app.use(bodyParser.json());
+app.use(cors())
+const Port = 3001 || process.env.PORT
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
+
+app.get("/", (req, res) => {
+
+    res.send("all good");
+})
+
+app.post('/chat-bot', async(req, res) => {
+    
+     const question = req.body.question
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            max_tokens :512 ,
+            temperature:0,
+            prompt: question,
+        });
+        const answer = completion.data.choices[0].text
+        res.status(200).json({
+            status : "success",
+            answer : answer
+        })
+    } catch (error) {
+       res.status(500).json({
+        status : "failed",
+        message : error.message
+       })
+    }
+})
+
+app.listen(Port, () => { console.log(`server is runnig on ${Port}`) });
